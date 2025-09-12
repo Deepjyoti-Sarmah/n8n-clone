2e02116f3980dbe1ae0c16e92d92f05d77536341
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-09-12 20:50:34 +0530

    fixed zod import errors

--- apps/server/src/api/v1/routes/users/signup.ts
+++ apps/server/src/api/v1/routes/users/signup.ts
@@ -1,7 +1,57 @@
 import { Hono, type Context } from "hono";
+import { signupSchema } from "@repo/zod";
+import { prisma } from "@repo/db";
 
 const signup = new Hono();
 
-signup.post(async (c: Context) => {});
+signup.post(async (c: Context) => {
+  const result = signupSchema.safeParse(c.body);
+  if (!result.success) {
+    return c.json(
+      {
+        message: "Invalid credentials",
+        error: result.error.issues,
+      },
+      400,
+    );
+  }
+
+  try {
+    const newUser = result.data;
+
+    if (!newUser) {
+      return c.json(
+        {
+          message: "Not enough details",
+        },
+        404,
+      );
+    }
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email: newUser.email,
+      },
+    });
+
+    if (existingUser) {
+      return c.json(
+        {
+          message: "User already exists",
+        },
+        409,
+      );
+    }
+  } catch (error) {
+    console.error("Cannot singup user", error);
+    return c.json(
+      {
+        message: "Internal server error",
+        error: error,
+      },
+      500,
+    );
+  }
+});
 
 export default signup;
