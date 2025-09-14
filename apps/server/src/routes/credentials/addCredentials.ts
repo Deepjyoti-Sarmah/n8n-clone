a08b94db4dad877b5818f1236dfa2c9c97a8625d
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-09-14 20:47:21 +0530

    adding credential routes

--- apps/server/src/routes/credentials/addCredentials.ts
+++ apps/server/src/routes/credentials/addCredentials.ts
@@ -1,11 +1,61 @@
-import { Hono } from "hono";
+import { Hono, type Context } from "hono";
+import { addCredentialsSchema } from "@repo/commons";
+import type { Variables } from "../../middlewares/auth";
+import { prisma } from "@repo/db";
 
-const addCredentials = new Hono();
+const addCredentials = new Hono<{ Variables: Variables }>();
 
-addCredentials.post("/", (c) => {
-  return c.json({
-    message: "Adding credentials",
-  });
+addCredentials.post("/", async (c: Context<{ Variables: Variables }>) => {
+  const result = addCredentialsSchema.safeParse(c.req.json());
+  if (!result.success) {
+    return c.json(
+      {
+        message: "Validation failed",
+        errors: result.error.issues,
+      },
+      400,
+    );
+  }
+
+  try {
+    const data = result.data;
+    if (!data) {
+      return c.json(
+        {
+          message: "Not enough details",
+        },
+        404,
+      );
+    }
+
+    const userId = c.get("userId");
+
+    const credentials = await prisma.credentials.create({
+      data: {
+        title: data.title,
+        platform: data.platform,
+        data: data.data,
+        userId,
+      },
+    });
+
+    return c.json(
+      {
+        message: "Credentials added successfully",
+        credentials: credentials,
+      },
+      200,
+    );
+  } catch (error) {
+    console.error("Error adding credentials", error);
+    return c.json(
+      {
+        message: "Internal server error",
+        error: error,
+      },
+      500,
+    );
+  }
 });
 
 export default addCredentials;
--- apps/server/src/routes/credentials/getCredentials.ts
+++ apps/server/src/routes/credentials/getCredentials.ts
@@ -1,11 +1,45 @@
-import { Hono } from "hono";
+import { Hono, type Context } from "hono";
+import type { Variables } from "../../middlewares/auth";
+import { prisma } from "@repo/db";
 
-const getCredentials = new Hono();
+const getCredentials = new Hono<{ Variables: Variables }>();
 
-getCredentials.get("/", (c) => {
-  return c.json({
-    message: "Get user credentials",
-  });
+getCredentials.get("/", async (c: Context<{ Variables: Variables }>) => {
+  try {
+    const userId = c.get("userId");
+
+    const credentials = await prisma.credentials.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    if (!credentials) {
+      return c.json(
+        {
+          message: "Credentials not found",
+        },
+        400,
+      );
+    }
+
+    return c.json(
+      {
+        message: "Credentials successfully fetched",
+        credentials: credentials,
+      },
+      200,
+    );
+  } catch (error) {
+    console.error("Error getting credentials");
+    return c.json(
+      {
+        message: "Internal server error",
+        error: error,
+      },
+      500,
+    );
+  }
 });
 
 getCredentials.get("/:credentialId", (c) => {
