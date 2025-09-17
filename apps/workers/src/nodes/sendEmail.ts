479f668051f6e183bf17caecd8d3a14cabb57096
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-09-17 23:34:16 +0530

    adding telegram and resend nodes

--- apps/workers/src/nodes/sendEmail.ts
+++ apps/workers/src/nodes/sendEmail.ts
@@ -1,9 +1,44 @@
+import { prisma } from "@repo/db";
+import { Resend } from "resend";
+import Mustache from "mustache";
+
 export const sendEmail = async (
   config: any,
   credentialsId: string,
   context: Record<string, any>,
 ) => {
   try {
+    const creds = await prisma.credentials.findUnique({
+      where: {
+        id: credentialsId,
+      },
+    });
+    if (!creds) {
+      throw new Error("Email credentials not found");
+    }
+
+    const data =
+      typeof creds.data === "string" ? JSON.parse(creds.data) : creds.data;
+    if (!data.apiKey) {
+      throw new Error("Email API key missing in credentials");
+    }
+
+    const resend = new Resend(data.apiKey);
+
+    const to = Mustache.render(config.to, context);
+    const subject = Mustache.render(config.subject, context);
+    const body = Mustache.render(config.body, context);
+
+    const result = await resend.emails.send({
+      from: data.resendDomainMail || "onboarding@resend.dev",
+      to,
+      subject,
+      html: body,
+    });
+
+    console.log(result);
+
+    return { to, subject, body };
   } catch (error) {
     console.error("Telegram node error", error);
     throw error;
--- apps/workers/src/nodes/telegram.ts
+++ apps/workers/src/nodes/telegram.ts
@@ -1,9 +1,69 @@
+import { prisma } from "@repo/db";
+import { fetch } from "bun";
+import Mustach from "mustache";
+
+type TelegramCred = {
+  botToken: string;
+  chatId: string;
+};
+
 export const sendTelegramMessage = async (
   config: any,
   credentialsId: string,
   context: Record<string, any>,
 ) => {
   try {
+    const creds = await prisma.credentials.findUnique({
+      where: {
+        id: credentialsId,
+      },
+    });
+
+    if (!creds) {
+      throw new Error("Telegram credentials not found");
+    }
+
+    const data =
+      typeof creds.data === "string" ? JSON.parse(creds.data) : creds.data;
+
+    const { botToken, chatId } = data as TelegramCred;
+    if (!botToken || !chatId) {
+      throw new Error("Telegram credentials invalid");
+    }
+
+    const message = Mustach.render(config.message, context);
+
+    const resp = await fetch(
+      `https://api.telegram.org/bot${botToken}/sendMessage`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          chat_id: chatId,
+          text: message,
+        }),
+      },
+    );
+
+    console.log("Telegram status: ", resp.status);
+
+    const text = await resp.text();
+    console.log("Telegram raw response:", text);
+
+    let result: any = {};
+    try {
+      result = JSON.parse(text);
+    } catch (error) {
+      throw new Error(`Telegram returned invalid JSON: ${text}`);
+    }
+
+    if (!result.ok) {
+      throw new Error(
+        `Telegram API error: ${result.description || JSON.stringify(result)}`,
+      );
+    }
+
+    return { message };
   } catch (error) {
     console.error("Telegram node error", error);
     throw error;
