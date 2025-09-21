78f2332e21cb9ed05b938cd1530cec83bba73b66
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-09-21 21:04:20 +0530

    updating runner node to add gemini

--- apps/executor/src/nodes/runner.ts
+++ apps/executor/src/nodes/runner.ts
@@ -1,12 +1,21 @@
+import { runGeminiNode } from "./gemini/gemini";
 import { sendEmail } from "./sendEmail";
 import { sendTelegramMessage } from "./telegram";
 
 export const runNode = async (
   node: any,
   context: Record<string, any>,
-  workflow?: string,
+  workflowId?: string,
 ) => {
   try {
+    if (!node.type) {
+      throw new Error("Node type is missing");
+    }
+
+    if (!node.credentialsId) {
+      throw new Error("Node credentials ID is missing");
+    }
+
     switch (node.type) {
       case "ResendEmail":
         return await sendEmail(node.config, node.credentialsId, context);
@@ -17,7 +26,7 @@ export const runNode = async (
           context,
         );
       case "Gemini":
-        return await run;
+        return await runGeminiNode(node, context, workflowId);
 
       default:
         throw new Error(`Unknown node type: ${node.type}`);
