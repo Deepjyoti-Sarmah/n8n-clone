661016596d433e5f511c0375dc13dbcbf8d8bef3
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-09-22 01:10:00 +0530

    adding sadcn and tailwindcss to react

---
+++ apps/front-end/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline text-yellow-300">
+          <Button>Hi there!</Button>
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
