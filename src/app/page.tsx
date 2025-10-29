b7a7fa6f267c6b0c307208a942d584e4a3f87db0
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-10-29 16:04:58 +0530

    Auth using better auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-const Page = (props: {}) => {
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
+import { LogoutButton } from "./logout";
+
+const Page = async () => {
+  await requireAuth();
+
+  const data = await caller.getUsers();
+
   return (
-    <div className="text-red-500">
-      Hello N8N
+    <div className="min-h-screen min-w-screen flex items-center justify-center  flex-col gap-y-6">
+      protected server component
+      <div>{JSON.stringify(data)}</div>
+      <LogoutButton />
     </div>
-  )
-}
+  );
+};
 
 export default Page;
