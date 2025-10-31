c7939cbdd923e805b50d9846b6b38f1ed7e72579
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-10-31 12:02:07 +0530

    added polar test payement and trials

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,14 +1,15 @@
-import { auth } from '@/lib/auth';
-import { initTRPC, TRPCError } from '@trpc/server';
-import next from 'next';
-import { headers } from 'next/headers';
-import { cache } from 'react';
+import { auth } from "@/lib/auth";
+import { polarClient } from "@/lib/polar";
+import { initTRPC, TRPCError } from "@trpc/server";
+import next from "next";
+import { headers } from "next/headers";
+import { cache } from "react";
 
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
    */
-  return { userId: 'user_123' };
+  return { userId: "user_123" };
 });
 
 // Avoid exporting the entire t-object
@@ -28,15 +29,35 @@ export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
 export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
   const session = await auth.api.getSession({
-    headers: await headers()
+    headers: await headers(),
   });
 
   if (!session) {
     throw new TRPCError({
       code: "UNAUTHORIZED",
-      message: "UNAUTHORIZED"
-    })
+      message: "UNAUTHORIZED",
+    });
   }
 
   return next({ ctx: { ...ctx, auth: session } });
-})
+});
+
+export const premiumProcedure = protectedProcedure.use(
+  async ({ ctx, next }) => {
+    const customer = await polarClient.customers.getStateExternal({
+      externalId: ctx.auth.user.id,
+    });
+
+    if (
+      !customer.activeSubscriptions ||
+      customer.activeSubscriptions.length === 0
+    ) {
+      throw new TRPCError({
+        code: "FORBIDDEN",
+        message: "Active subscription required",
+      });
+    }
+
+    return next({ ctx: { ...ctx, customer } });
+  },
+);
