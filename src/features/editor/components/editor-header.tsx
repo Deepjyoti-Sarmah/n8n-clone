1702a4021d9b3ba500ae971f4528df601e3a16e0
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-11-04 01:25:16 +0530

    adding editor ui

---
+++ src/features/editor/components/editor-header.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { SaveIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+import {
+  useSuspenseWorkflow,
+  useUpdateWorkflowName,
+} from "@/features/workflows/hooks/use-workflows";
+
+export const EditorSaveButton = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <div className="ml-auto">
+      <Button size="sm" onClick={() => {}} disabled={false}>
+        <SaveIcon className="size-4" />
+        Save
+      </Button>
+    </div>
+  );
+};
+
+export const EditorNameInput = ({ workflowId }: { workflowId: string }) => {
+  const { data: workflow } = useSuspenseWorkflow(workflowId);
+  const updateWorkflow = useUpdateWorkflowName();
+
+  return <BreadcrumbItem>{workflow?.name}</BreadcrumbItem>;
+};
+
+export const EditorBreadcrumbs = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link prefetch href="/workflows">
+              Workflows
+            </Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <EditorNameInput workflowId={workflowId} />
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export const EditorHeader = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <header className="flex h-14 shrink-0 items-center gap-2 border-b px-4 bg-background">
+      <SidebarTrigger />
+      <div className="flex flex-row items-center justify-between gap-x-4 w-full">
+        <EditorBreadcrumbs workflowId={workflowId} />
+        <EditorSaveButton workflowId={workflowId} />
+      </div>
+    </header>
+  );
+};
