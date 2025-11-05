eac73edf37c7ee0ce39d70cfa35d134056e85ccf
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-11-05 16:28:38 +0530

    added trigger and manual node selector

--- src/components/node-selector.tsx
+++ src/components/node-selector.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { createId } from "@paralleldrive/cuid2";
+import { useReactFlow } from "@xyflow/react";
 import { GlobeIcon, MousePointerIcon } from "lucide-react";
-import React from "react";
+import type React from "react";
+import { useCallback } from "react";
+import { toast } from "sonner";
 import { NodeType } from "@/generated/prisma/enums";
 import { Separator } from "./ui/separator";
 import {
@@ -50,6 +54,54 @@ export function NodeSelector({
   onOpenChnage,
   children,
 }: NodeSelectorProps) {
+  const { setNodes, getNodes, screenToFlowPosition } = useReactFlow();
+
+  const handleNodeSelect = useCallback(
+    (selection: NodeTypeOption) => {
+      if (selection.type === NodeType.MANUAL_TRIGGER) {
+        const nodes = getNodes();
+        const hasManualTrigger = nodes.some(
+          (node) => node.type === NodeType.MANUAL_TRIGGER,
+        );
+
+        if (hasManualTrigger) {
+          toast.error("Only one manual trigger is allowed per workflow");
+          return;
+        }
+      }
+
+      setNodes((nodes) => {
+        const hasInitialTrigger = nodes.some(
+          (node) => node.type === NodeType.INITIAL,
+        );
+
+        const centerX = window.innerWidth / 2;
+        const centerY = window.innerHeight / 2;
+
+        const flowPosition = screenToFlowPosition({
+          x: centerX + (Math.random() - 0.5) * 200,
+          y: centerY + (Math.random() - 0.5) * 200,
+        });
+
+        const newNode = {
+          id: createId(),
+          data: {},
+          position: flowPosition,
+          type: selection.type,
+        };
+
+        if (hasInitialTrigger) {
+          return [newNode];
+        }
+
+        return [...nodes, newNode];
+      });
+
+      onOpenChnage(false);
+    },
+    [setNodes, getNodes, onOpenChnage, screenToFlowPosition],
+  );
+
   return (
     <Sheet open={open} onOpenChange={onOpenChnage}>
       <SheetTrigger asChild>{children}</SheetTrigger>
@@ -67,7 +119,7 @@ export function NodeSelector({
               <div
                 key={nodeType.type}
                 className="w-full justify-start h-auto py-5 px-4 rounded-none cursor-pointer border-l-2 border-transparent hover:border-l-primary"
-                // onClick={() => {}}
+                onClick={() => handleNodeSelect(nodeType)}
               >
                 <div className="flex items-center gap-6 w-full overflow-hidden">
                   {typeof Icon === "string" ? (
@@ -102,7 +154,7 @@ export function NodeSelector({
               <div
                 key={nodeType.type}
                 className="w-full justify-start h-auto py-5 px-4 rounded-none cursor-pointer border-l-2 border-transparent hover:border-l-primary"
-                // onClick={() => {}}
+                onClick={() => handleNodeSelect(nodeType)}
               >
                 <div className="flex items-center gap-6 w-full overflow-hidden">
                   {typeof Icon === "string" ? (
