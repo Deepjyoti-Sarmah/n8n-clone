6a03a270e391be4c88de29daabe1ab4c34dad528
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-11-14 01:03:18 +0530

    fixing variable type error

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -3,9 +3,9 @@ import { NonRetriableError } from "inngest";
 import ky, { Options as KyOptions } from "ky";
 
 type HttpRequestData = {
-  variableName?: string;
-  endpoint?: string;
-  method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+  variableName: string;
+  endpoint: string;
+  method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
   body?: string;
 };
 
@@ -24,12 +24,19 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
 
   if (!data.variableName) {
     //TODO: Publish "error" state for http request
-    throw new NonRetriableError("Variable name not configured");
+    throw new NonRetriableError(
+      "HTTP Request node: Variable name not configured",
+    );
+  }
+
+  if (!data.method) {
+    //TODO: Publish "error" state for http request
+    throw new NonRetriableError("HTTP Request node: Method not configured");
   }
 
   const result = await step.run("http-request", async () => {
-    const endpoint = data.endpoint!;
-    const method = data.method || "GET";
+    const endpoint = data.endpoint;
+    const method = data.method;
 
     const options: KyOptions = { method };
 
@@ -54,17 +61,9 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       },
     };
 
-    if (data.variableName) {
-      return {
-        ...context,
-        [data.variableName]: responsePayload,
-      };
-    }
-
-    // fallback to direct httpResponse for backware compatibility
     return {
       ...context,
-      ...responsePayload,
+      [data.variableName]: responsePayload,
     };
   });
 
