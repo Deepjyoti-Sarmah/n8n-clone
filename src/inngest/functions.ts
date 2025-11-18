e32e70458ef3a205fd16103cc8904f5a97b17871
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-11-19 01:28:24 +0530

    added gemini node

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -8,6 +8,7 @@ import { manualTriggerChannel } from "./channels/manual-trigger";
 import { inngest } from "./client";
 import { topologicalSort } from "./utils";
 import { stripeTriggerChannel } from "./channels/stripe-trigger";
+import { geminiChannel } from "./channels/gemini";
 
 export const executeWorkflow = inngest.createFunction(
   {
@@ -21,6 +22,7 @@ export const executeWorkflow = inngest.createFunction(
       manualTriggerChannel(),
       googleFormTriggerChannel(),
       stripeTriggerChannel(),
+      geminiChannel(),
     ],
   },
   async ({ event, step, publish }) => {
