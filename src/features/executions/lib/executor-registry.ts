354f65a9070c2194ce6f9b8fdbeddd4493f6d750
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-11-18 13:25:22 +0530

    solved httpRequest type errors

--- src/features/executions/lib/executor-registry.ts
+++ src/features/executions/lib/executor-registry.ts
@@ -7,7 +7,7 @@ import type { NodeExecutor } from "../types";
 export const executorRegistry: Record<NodeType, NodeExecutor> = {
   [NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
   [NodeType.INITIAL]: manualTriggerExecutor,
-  [NodeType.HTTP_REQUEST]: httpRequestExecutor, //TODO: fix types
+  [NodeType.HTTP_REQUEST]: httpRequestExecutor,
   [NodeType.GOOGLE_FORM_TRIGGER]: googleFormTriggerExecutor,
 };
 
