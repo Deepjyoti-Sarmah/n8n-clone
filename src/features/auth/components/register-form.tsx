7f11c1924f5823d76052b7e26c7aa7f0c4a293fa
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-11-25 13:36:56 +0530

    Added google-auth and github-auth

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -52,6 +52,38 @@ export function RegisterForm() {
     },
   });
 
+  const signInGithub = async () => {
+    await authClient.signIn.social(
+      {
+        provider: "github",
+      },
+      {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: () => {
+          toast.error("Something went wrong");
+        },
+      },
+    );
+  };
+
+  const signInGoogle = async () => {
+    await authClient.signIn.social(
+      {
+        provider: "google",
+      },
+      {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: () => {
+          toast.error("Something went wrong");
+        },
+      },
+    );
+  };
+
   const onSubmit = async (values: RegisterFormValues) => {
     await authClient.signUp.email(
       {
@@ -87,6 +119,7 @@ export function RegisterForm() {
               <div className="grid gap-6">
                 <div className="flex flex-col gap-4">
                   <Button
+                    onClick={signInGithub}
                     variant="outline"
                     type="button"
                     className="w-full"
@@ -102,6 +135,7 @@ export function RegisterForm() {
                   </Button>
 
                   <Button
+                    onClick={signInGoogle}
                     variant="outline"
                     type="button"
                     className="w-full"
